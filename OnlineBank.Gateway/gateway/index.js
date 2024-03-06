76a58591c97d9c2b39238f631ba40a062548eaa9
Rapp Anna <rppanya@gmail.com>
2024-03-06 20:50:28 +0700

    deleted '/core-service' from path

--- OnlineBank.Gateway/gateway/index.js
+++ OnlineBank.Gateway/gateway/index.js
@@ -3,15 +3,18 @@ const app = express();
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const port = 3000;
 
-// http://127.0.0.1:3000/core-service => http://127.0.0.1:8080/
+// http://127.0.0.1:3000/ => http://127.0.0.1:8080/
 
 app.use(
-  "/core-service",
+  "/api/bank-accounts",
   createProxyMiddleware({
     target: "http://127.0.0.1:8080",
-    pathRewrite: {
-      "^/core-service": "",
-    },
+  })
+);
+app.use(
+  "/api/transactions",
+  createProxyMiddleware({
+    target: "http://127.0.0.1:8080",
   })
 );
 
