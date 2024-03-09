758773e23071abfc254df33cdd7756c30ea0cc75
danka <dankagrasmik@gmail.com>
2024-03-09 23:12:15 +0700

    id in loan rate dto

--- OnlineBank.LoanService.Common/Dtos/LoanRate/LoanRateDto.cs
+++ OnlineBank.LoanService.Common/Dtos/LoanRate/LoanRateDto.cs
@@ -2,6 +2,7 @@ namespace OnlineBank.LoanService.Common.Dtos.LoanRate;
 
 public class LoanRateDto
 {
+    public Guid id { get; set; }
     public string name { get; set; }
     public double interestRate { get; set; }
 }
