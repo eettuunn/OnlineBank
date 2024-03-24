f09959cbc0f9ee16ba8b16ac62ec1417bd0ef9b6
Rapp Anna <rppanya@gmail.com>
2024-03-24 16:11:42 +0700

    added styles for themes

--- OnlineBank.StaffPanel/src/pages/MainLayout.tsx
+++ OnlineBank.StaffPanel/src/pages/MainLayout.tsx
@@ -22,6 +22,16 @@ const MainLayout: React.FC = () => {
 
     const [ darkTheme, setDarkTheme ] = useLocalStorage('dark-theme', false);
 
+    useEffect(() => {
+        if (darkTheme) {
+            document.body.classList.remove('light');
+            document.body.classList.add('dark');
+        } else {
+            document.body.classList.remove('dark');
+            document.body.classList.add('light');
+        }
+    }, [ darkTheme ]);
+
     return (
         <Layout>
             <Sider
@@ -30,7 +40,6 @@ const MainLayout: React.FC = () => {
                 className={b('sider').toString()}
                 collapsed={collapsed}
                 collapsedWidth={88}
-                theme="light"
                 trigger={null}
                 width={320}
                 onCollapse={setCollapsed}
